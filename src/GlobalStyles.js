db13eb01dd41190a0c8318ccc496837e582b1d6a
Igor <igormgg@hotmail.com>
2021-10-28 19:56:05 -0300

    fix: root colors fixed

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,13 +5,13 @@ const GlobalStyle = createGlobalStyle`
     --color-primary: #27AE60;
     --color-secondary: #EB5757;
     --gray-600: #27ae60;
-    --gray-300: #EB5757;
-    --grey-100: #eb5757;
-    --grey-0: #f5f5f5;
-    --fb-negative: #e60000;
+    --gray-300: #828282;
+    --grey-100: #E0E0E0;
+    --grey-0: #F5F5F5;
+    --fb-negative: #E60000;
     --fb-warning: #FFCD07;
     --fb-success: #168821;
-    --fb-information: #155bcb;
+    --fb-information: #155BCB;
 }
 
 
